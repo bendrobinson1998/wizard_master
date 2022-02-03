88004dd0bb57bcf68b8e126373ca95f35625ad8d
bendrobinson1998 <benjidrobinson@gmail.com>
2022-02-03 18:08:44 +0000

    smooth scroll polyfill implementation

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import HomePage from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mint from "./pages/Mint";
+import { useEffect } from "react";
 
+import smoothscroll from "smoothscroll-polyfill";
 function App() {
+  useEffect(() => {
+    // kick off the polyfill!
+    smoothscroll.polyfill();
+  }, []);
   return (
     <>
-      
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/mint" element={<Mint />} />
-            <Route path="*" element={<HomePage />} />
-          </Routes>
-        </BrowserRouter>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/mint" element={<Mint />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
