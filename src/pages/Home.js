90a3787d4bad4a23f4e9fab7e8d5a43ff3dc3e3c
bendrobinson1998 <benjidrobinson@gmail.com>
2022-02-03 14:18:55 +0000

    navbar delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 import Hero from "../components/page_sections/Hero";
-import Navbar from "../components/page_sections/Navbar";
+/* import Navbar from "../components/page_sections/Navbar"; */
 import Legendary from "../components/page_sections/Legendary";
 import Roadmap from "../components/page_sections/Roadmap";
 import Token from "../components/page_sections/Token";
 import Metaverse from "../components/page_sections/Metaverse";
 import Launchpad from "../components/page_sections/Launchpad";
 import Team from "../components/page_sections/Team";
-import MetaverseNew from "../components/page_sections/MetaverseNew";
+
 import Discord from "../components/page_sections/Discord";
 
 export default function HomePage() {
   return (
     <div className="app">
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Legendary />
       <Metaverse />
