c250ab9ff5de256a2f37bbef5d4ab523269208ae
bendrobinson1998 <benjidrobinson@gmail.com>
2022-02-24 13:52:47 +0000

    console log clear up

--- src/components/accessories/Particles.js
+++ src/components/accessories/Particles.js
@@ -2,14 +2,10 @@ import React from "react";
 import Particles from "react-tsparticles";
 const Particle = () => {
   const particlesInit = (main) => {
-    console.log(main);
-
     // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
   };
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+  const particlesLoaded = (container) => {};
 
   return (
     <Particles
